33cd434428016b72250b2a38356000f9c3bc52ae
Pavlo Kovalenko <rabota.pashok@gmail.com>
2025-12-02 11:57:36 +0200

    debug logs server

--- server/bin/www.mjs
+++ server/bin/www.mjs
@@ -8,6 +8,7 @@ const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 
 const server = http.createServer(app)
+console.log(port)
 server.listen(port)
 server.on('error', onError)
 server.on('listening', onListening)
