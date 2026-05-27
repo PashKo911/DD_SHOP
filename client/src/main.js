85f40cfe685dce57f39724e8aa2bf26189e1d81a
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-05-27 13:06:09 +0300

    refactor: use router state as single source of locale in app bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -16,34 +16,26 @@ import { useCommonStore } from './stores/common'
 import { initI18n } from './plugins/i18n'
 import detectLocale from './utils/locale/detectLocale'
 
-function bootstrap() {
-	const app = createApp(App)
-	const pinia = createPinia()
+const app = createApp(App)
+const pinia = createPinia()
 
-	app.use(pinia)
+app.use(pinia)
 
-	app.use(router)
+app.use(router)
 
-	const locale = detectLocale({
-		params: {
-			locale: window.location.pathname.match(/^\/(en|uk)(?:\/|$)/)?.[1],
-		},
-	})
+const locale = detectLocale(router.currentRoute.value)
 
-	const i18n = initI18n(locale)
-	const commonStore = useCommonStore()
-	commonStore.setLocale(locale)
-	app.use(i18n)
+const i18n = initI18n(locale)
+const commonStore = useCommonStore()
+commonStore.setLocale(locale)
+app.use(i18n)
 
-	const head = createHead()
-	app.use(head)
-	app.use(PrimeVue, { unstyled: true })
-	app.use(ToastService)
+const head = createHead()
+app.use(head)
+app.use(PrimeVue, { unstyled: true })
+app.use(ToastService)
 
-	app.directive('lockScroll', lockScroll)
-	app.directive('styleclass', StyleClass)
+app.directive('lockScroll', lockScroll)
+app.directive('styleclass', StyleClass)
 
-	app.mount('#app')
-}
-
-bootstrap()
+app.mount('#app')
